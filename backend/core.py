418a0a0813f5b363ee78527f4534f65e104ae7f6
yeha98555 <yeha98555@gmail.com>
2023-08-15 16:44:56 +0800

    Add RetrievalQA chain

---
+++ backend/core.py
@@ -0,0 +1,32 @@
+import os
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Pinecone
+import pinecone
+
+
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_API_ENV"],
+)
+
+
+def run_llm(query: str) -> any:
+    embeddings = OpenAIEmbeddings()
+    docsearch = Pinecone.from_existing_index(
+        index_name=os.environ["PINECONE_INDEX_NAME"], embedding=embeddings
+    )
+    chat = ChatOpenAI(verbose=True, temperature=0)
+    qa = RetrievalQA.from_chain_type(
+        llm=chat,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True,
+    )
+    return qa({"query": query})
+
+
+if __name__ == "__main__":
+    print(run_llm(query="What is LangChain?"))
